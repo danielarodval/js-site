f393fc0f6b6b09766483ce817135492b46f20d6d
danielarodval <49764316+danielarodval@users.noreply.github.com>
2024-01-08 21:48:04 -0500

    favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab5eb9b03e39f17e"; }
+/******/ 		__webpack_require__.h = function() { return "f97218c0cfb0a563"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,21 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
+import Head from 'next/head'
 
 const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
+            <Head>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+
             <Fonts />
+
             <Layout router={router}>
                 <Component {...pageProps} key={router.route} />
             </Layout>
+
         </ChakraProvider>
     )
 }
