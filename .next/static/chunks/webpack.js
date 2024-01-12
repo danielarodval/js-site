1b1fe35226b5fee20c84358c9ccb84c7b09357d3
danielarodval <49764316+danielarodval@users.noreply.github.com>
2024-01-11 20:16:25 -0500

    updated package.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5fdf74ec947de564"; }
+/******/ 		__webpack_require__.h = function() { return "ff9d901c455d29fe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
